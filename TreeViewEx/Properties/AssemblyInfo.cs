d62173507bc31ecbbb224d80f3a0d4987cc63590
jkuehner <jochen.kuehner@kardex.com>
2020-03-15 22:53:04 +0100

    netcore support

--- TreeViewEx/Properties/AssemblyInfo.cs
+++ TreeViewEx/Properties/AssemblyInfo.cs
@@ -4,23 +4,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: InternalsVisibleTo("TreeViewEx.Test")]
-[assembly: AssemblyTitle("TreeViewEx")]
-[assembly: AssemblyDescription("Library for a multi select TreeView")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("-")]
-[assembly: AssemblyProduct("TreeViewEx")]
-[assembly: AssemblyCopyright("Copyright © Goroll 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
     //(used if a resource is not found in the page, 
